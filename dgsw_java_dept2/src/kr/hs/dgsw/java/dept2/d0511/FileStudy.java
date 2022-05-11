369edc6b512adfde383d303238e889f75e1d303d
Woogong <batang21@gmail.com>
2022-05-11 14:52:16 +0900

    ..

--- dgsw_java_dept2/src/kr/hs/dgsw/java/dept2/d0511/FileStudy.java
+++ dgsw_java_dept2/src/kr/hs/dgsw/java/dept2/d0511/FileStudy.java
@@ -47,8 +47,73 @@ public class FileStudy {
 		System.out.println(name + "  " + formatDate(convertToDate(time)));
 		System.out.println(file2.getName() + "  " + file2.lastModified());
 		System.out.println();
+	}
+
+	public void studyManage() throws Exception {
+		/* 파일 만들기 */
+		File file = new File("C:/90_ETC/javaStudy/hello.txt");
+		boolean created = file.createNewFile();
+		System.out.println("파일 만들기");
+		System.out.println(file.getName() + "  " + created);
+		System.out.println();
 		
+		/* 파일 삭제하기 */
+		boolean deleted = file.delete();
+		System.out.println("파일 삭제하기");
+		System.out.println(file.getName() + "  " + deleted);
+		System.out.println();
 		
+		/* 디렉토리 만들기 */
+		File dir = new File("C:/90_ETC/javaStudy/subDir1");
+		created = dir.mkdir();
+		System.out.println("디렉토리 만들기");
+		System.out.println(dir.getName() + "  " + created);
+		System.out.println();
+		
+		/* 디렉토리 삭제하기 */
+		deleted = dir.delete();
+		System.out.println("디렉토리 삭제하기");
+		System.out.println(dir.getName() + "  " + deleted);
+		System.out.println();
+		
+		/* 디렉토리 만들기 2 */
+		File dir2 = new File("C:/90_ETC/javaStudy/child/images");
+		created = dir2.mkdirs();
+		System.out.println("디렉토리 만들기2");
+		System.out.println(dir2.getName() + "  " + created);
+		System.out.println();
+		
+		/* 파일과 디렉토리 구분하기 */
+		File some1 = new File("C:/90_ETC/javaStudy/sample.txt");
+		File some2 = new File("C:/90_ETC/javaStudy/child");
+		File some3 = new File("C:/90_ETC/javaStudy/void");
+		
+		System.out.println("파일과 디렉토리 구분하기");
+		System.out.println(some1.getName() + "  " + some1.isFile() + "  " + some1.isDirectory());
+		System.out.println(some2.getName() + "  " + some2.isFile() + "  " + some2.isDirectory());
+		System.out.println(some3.getName() + "  " + some3.isFile() + "  " + some3.isDirectory());
+	
+		/* 파일의 이름 바꾸기 */
+		File sample = new File("C:/90_ETC/javaStudy/sample.txt");
+		File newName = new File("C:/90_ETC/javaStudy/child/서시.txt");
+		sample.renameTo(newName);
+		
+	}
+	
+	public void studyList() {
+		File dir = new File("C:/");
+		
+		String[] children = dir.list();
+		for (String name : children) {
+			System.out.println(name);
+		}
+		
+		System.out.println("-------------------------");
+		
+		File[] childrenFiles = dir.listFiles();
+		for (File file : childrenFiles) {
+			System.out.println(file.getAbsolutePath());
+		}
 	}
 	
 	public void showFileData(File file) {
@@ -67,10 +132,11 @@ public class FileStudy {
 	
 	public static void main(String[] args) throws Exception {
 		FileStudy fileStudy = new FileStudy();
-		//fileStudy.studyFileData();
+		fileStudy.studyManage();
+		//fileStudy.studyList();
 		
-		File file = new File("C:/90_ETC/javaStudy/sample.txt");
-		fileStudy.showFileData(file);
+		/*File file = new File("C:/90_ETC/javaStudy/sample.txt");
+		fileStudy.showFileData(file);*/
 	}
 	
 }
